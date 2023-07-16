ef31d3aa07118f4c7777a4ae67bbdbe35fa41c9f
gerkone <gianlucagalletti@ymail.com>
2023-07-16 15:40:06 +0200

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,15 @@ import pytest
 
 from segnn_jax import SteerableGraphsTuple
 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 
 @pytest.fixture
 def dummy_graph():
-    def _rand_graph(n_graphs: int = 1):
+    def _rand_graph(n_graphs: int = 1, attr_irreps: str = "1x0e + 1x1o"):
+        attr_irreps = e3nn.Irreps(attr_irreps)
         return SteerableGraphsTuple(
             graph=jraph.GraphsTuple(
                 nodes=e3nn.IrrepsArray("1x1o", jnp.ones((n_graphs * 5, 3))),
@@ -27,7 +29,9 @@ def dummy_graph():
             ),
             additional_message_features=None,
             edge_attributes=None,
-            node_attributes=e3nn.IrrepsArray("1x0e+1x1o", jnp.ones((n_graphs * 5, 4))),
+            node_attributes=e3nn.IrrepsArray(
+                attr_irreps, jnp.ones((n_graphs * 5, attr_irreps.dim))
+            ),
         )
 
     return _rand_graph
--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -3,13 +3,21 @@ import haiku as hk
 import pytest
 from e3nn_jax.utils import assert_equivariant
 
-from segnn_jax import O3TensorProduct, O3TensorProductFC, O3TensorProductGate
+from segnn_jax import (
+    O3TensorProduct,
+    O3TensorProductFC,
+    O3TensorProductGate,
+    O3TensorProductSCN,
+)
 
 
 @pytest.mark.parametrize("biases", [False, True])
-def test_linear(key, biases):
+@pytest.mark.parametrize(
+    "O3Layer", [O3TensorProduct, O3TensorProductFC, O3TensorProductSCN]
+)
+def test_linear_layers(key, biases, O3Layer):
     def f(x1, x2):
-        return O3TensorProduct("1x1o", biases=biases)(x1, x2)
+        return O3Layer("1x1o", biases=biases)(x1, x2)
 
     f = hk.without_apply_rng(hk.transform(f))
 
@@ -22,14 +30,18 @@ def test_linear(key, biases):
     assert_equivariant(
         wrapper,
         key,
-        args_in=(e3nn.normal("1x1o", key, (5,)), e3nn.normal("1x1o", key, (5,))),
+        e3nn.normal("1x1o", key, (5,)),
+        e3nn.normal("1x1o", key, (5,)),
     )
 
 
 @pytest.mark.parametrize("biases", [False, True])
-def test_linear_fully_connected(key, biases):
+@pytest.mark.parametrize(
+    "O3Layer", [O3TensorProduct, O3TensorProductFC, O3TensorProductSCN]
+)
+def test_gated_layers(key, biases, O3Layer):
     def f(x1, x2):
-        return O3TensorProductFC("1x1o", biases=biases)(x1, x2)
+        return O3TensorProductGate("1x1o", biases=biases, o3_layer=O3Layer)(x1, x2)
 
     f = hk.without_apply_rng(hk.transform(f))
 
@@ -42,51 +54,8 @@ def test_linear_fully_connected(key, biases):
     assert_equivariant(
         wrapper,
         key,
-        args_in=(e3nn.normal("1x1o", key, (5,)), e3nn.normal("1x1o", key, (5,))),
-    )
-
-
-@pytest.mark.parametrize("biases", [False, True])
-def test_gated(key, biases):
-    def f(x1, x2):
-        return O3TensorProductGate("1x1o", biases=biases, o3_layer=O3TensorProduct)(
-            x1, x2
-        )
-
-    f = hk.without_apply_rng(hk.transform(f))
-
-    v = e3nn.normal("1x1o", key, (5,))
-    params = f.init(key, v, v)
-
-    def wrapper(x1, x2):
-        return f.apply(params, x1, x2)
-
-    assert_equivariant(
-        wrapper,
-        key,
-        args_in=(e3nn.normal("1x1o", key, (5,)), e3nn.normal("1x1o", key, (5,))),
-    )
-
-
-@pytest.mark.parametrize("biases", [False, True])
-def test_linear_legacy(key, biases):
-    def f(x1, x2):
-        return O3TensorProductFC("1x1o", biases=biases, o3_layer=O3TensorProductFC)(
-            x1, x2
-        )
-
-    f = hk.without_apply_rng(hk.transform(f))
-
-    v = e3nn.normal("1x1o", key, (5,))
-    params = f.init(key, v, v)
-
-    def wrapper(x1, x2):
-        return f.apply(params, x1, x2)
-
-    assert_equivariant(
-        wrapper,
-        key,
-        args_in=(e3nn.normal("1x1o", key, (5,)), e3nn.normal("1x1o", key, (5,))),
+        e3nn.normal("1x1o", key, (5,)),
+        e3nn.normal("1x1o", key, (5,)),
     )
 
 
