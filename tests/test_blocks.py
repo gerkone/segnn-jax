281c6c3683e6f91d25ff5a47b6ac3be3d74433c8
gerkone <gianlucagalletti@ymail.com>
2023-07-16 12:38:10 +0200

    adapted tests

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,20 +1,43 @@
 import e3nn_jax as e3nn
 import haiku as hk
 import pytest
-from e3nn_jax.util import assert_equivariant
+from e3nn_jax.utils import assert_equivariant
 
-from segnn_jax import O3TensorProduct, O3TensorProductGate, O3TensorProductLegacy
+from segnn_jax import O3TensorProduct, O3TensorProductFC, O3TensorProductGate
 
 
 @pytest.mark.parametrize("biases", [False, True])
 def test_linear(key, biases):
-    f = lambda x1, x2: O3TensorProduct("1x1o", biases=biases)(x1, x2)
+    def f(x1, x2):
+        return O3TensorProduct("1x1o", biases=biases)(x1, x2)
+
     f = hk.without_apply_rng(hk.transform(f))
 
     v = e3nn.normal("1x1o", key, (5,))
     params = f.init(key, v, v)
 
-    wrapper = lambda x1, x2: f.apply(params, x1, x2)
+    def wrapper(x1, x2):
+        return f.apply(params, x1, x2)
+
+    assert_equivariant(
+        wrapper,
+        key,
+        args_in=(e3nn.normal("1x1o", key, (5,)), e3nn.normal("1x1o", key, (5,))),
+    )
+
+
+@pytest.mark.parametrize("biases", [False, True])
+def test_linear_fully_connected(key, biases):
+    def f(x1, x2):
+        return O3TensorProductFC("1x1o", biases=biases)(x1, x2)
+
+    f = hk.without_apply_rng(hk.transform(f))
+
+    v = e3nn.normal("1x1o", key, (5,))
+    params = f.init(key, v, v)
+
+    def wrapper(x1, x2):
+        return f.apply(params, x1, x2)
 
     assert_equivariant(
         wrapper,
@@ -25,17 +48,18 @@ def test_linear(key, biases):
 
 @pytest.mark.parametrize("biases", [False, True])
 def test_gated(key, biases):
-    import segnn_jax.blocks
+    def f(x1, x2):
+        return O3TensorProductGate("1x1o", biases=biases, o3_layer=O3TensorProduct)(
+            x1, x2
+        )
 
-    segnn_jax.blocks.O3Layer = segnn_jax.blocks.O3TensorProduct
-
-    f = lambda x1, x2: O3TensorProductGate("1x1o", biases=biases)(x1, x2)
     f = hk.without_apply_rng(hk.transform(f))
 
     v = e3nn.normal("1x1o", key, (5,))
     params = f.init(key, v, v)
 
-    wrapper = lambda x1, x2: f.apply(params, x1, x2)
+    def wrapper(x1, x2):
+        return f.apply(params, x1, x2)
 
     assert_equivariant(
         wrapper,
@@ -46,13 +70,18 @@ def test_gated(key, biases):
 
 @pytest.mark.parametrize("biases", [False, True])
 def test_linear_legacy(key, biases):
-    f = lambda x1, x2: O3TensorProductLegacy("1x1o", biases=biases)(x1, x2)
+    def f(x1, x2):
+        return O3TensorProductFC("1x1o", biases=biases, o3_layer=O3TensorProductFC)(
+            x1, x2
+        )
+
     f = hk.without_apply_rng(hk.transform(f))
 
     v = e3nn.normal("1x1o", key, (5,))
     params = f.init(key, v, v)
 
-    wrapper = lambda x1, x2: f.apply(params, x1, x2)
+    def wrapper(x1, x2):
+        return f.apply(params, x1, x2)
 
     assert_equivariant(
         wrapper,
--- tests/test_segnn.py
+++ tests/test_segnn.py
@@ -1,25 +1,24 @@
 import e3nn_jax as e3nn
 import haiku as hk
 import pytest
-from e3nn_jax.util import assert_equivariant
+from e3nn_jax.utils import assert_equivariant
 
-from segnn_jax import SEGNN, weight_balanced_irreps
+from segnn_jax import SEGNN, O3TensorProduct, O3TensorProductFC, weight_balanced_irreps
 
 
 @pytest.mark.parametrize("task", ["graph", "node"])
 @pytest.mark.parametrize("norm", ["none", "instance"])
 def test_equivariance(key, dummy_graph, norm, task):
-    import segnn_jax.blocks
+    def segnn(x):
+        return SEGNN(
+            hidden_irreps=weight_balanced_irreps(8, e3nn.Irreps.spherical_harmonics(1)),
+            output_irreps=e3nn.Irreps("1x1o"),
+            num_layers=1,
+            task=task,
+            norm=norm,
+            o3_layer=O3TensorProduct,
+        )(x)
 
-    segnn_jax.blocks.O3Layer = segnn_jax.blocks.O3TensorProduct
-
-    segnn = lambda x: SEGNN(
-        hidden_irreps=weight_balanced_irreps(8, e3nn.Irreps.spherical_harmonics(1)),
-        output_irreps=e3nn.Irreps("1x1o"),
-        num_layers=1,
-        task=task,
-        norm=norm,
-    )(x)
     segnn = hk.without_apply_rng(hk.transform_with_state(segnn))
 
     graph = dummy_graph()
@@ -38,18 +37,17 @@ def test_equivariance(key, dummy_graph, norm, task):
 
 @pytest.mark.parametrize("task", ["graph", "node"])
 @pytest.mark.parametrize("norm", ["none", "instance"])
-def test_equivariance_legacy(key, dummy_graph, norm, task):
-    import segnn_jax.blocks
+def test_equivariance_fully_connected(key, dummy_graph, norm, task):
+    def segnn(x):
+        return SEGNN(
+            hidden_irreps=weight_balanced_irreps(8, e3nn.Irreps.spherical_harmonics(1)),
+            output_irreps=e3nn.Irreps("1x1o"),
+            num_layers=1,
+            task=task,
+            norm=norm,
+            o3_layer=O3TensorProductFC,
+        )(x)
 
-    segnn_jax.blocks.O3Layer = segnn_jax.blocks.O3TensorProductLegacy
-
-    segnn = lambda x: SEGNN(
-        hidden_irreps=weight_balanced_irreps(8, e3nn.Irreps.spherical_harmonics(1)),
-        output_irreps=e3nn.Irreps("1x1o"),
-        num_layers=1,
-        task=task,
-        norm=norm,
-    )(x)
     segnn = hk.without_apply_rng(hk.transform_with_state(segnn))
 
     graph = dummy_graph()
